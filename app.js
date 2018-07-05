6e4fe96fe4b2aa416a335349e7ccab6f9b6006fe
riedelsolutions <38634824+riedelsolutions@users.noreply.github.com>
2018-07-05 03:08:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-
-
-		var fontCharacteristic = prompt("I want a font more: " ,"");
+var fontCharacteristic = prompt("I want a font more: " ,"");
 		var fontInput = prompt("than","");
 
-		var seriousnessArray = ["Helvetica", "Book Antiqua", "Open Sans", "Courier", "Arial", "Comic Sans"];
+		var seriousnessArray = ["Comic Sans", "Arial", "Courier", "Open Sans", "Book Antiqua", "Helvetica"];
 
 		var result = [];
 		var index;
@@ -19,13 +17,16 @@
 
 				}
 			}
-		} else{
+		} else {
 			document.write("Sorry, we could not find fonts more " + fontCharacteristic + " than " + fontInput + ".");
 		}
 
 		//We then get the fonts who are more or less of what the user want it, and create a resulting array with it
 
-		for (var i =index + 1; i < seriousnessArray.length; i++){
+		for (var i = index + 1; i < seriousnessArray.length; i++){
+			if (index >= seriousnessArray.length-1){
+				document.write("Sorry, we could not find fonts more" + fontCharacteristic +  " than " + fontInput + ".");
+			}
 			result[indexOfNewArray] = seriousnessArray[i];
 			indexOfNewArray++;
 		}
