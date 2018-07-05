cbc45ac17e8cf8666bd94b98d3949de456e4f43f
riedelsolutions <38634824+riedelsolutions@users.noreply.github.com>
2018-07-05 04:31:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 
-
 		var fontCharacteristic = prompt("I want a font more: " ,"");
 		var fontInput = prompt("than","");
 
-		var seriousnessArray = ["Comic Sans", "Arial", "Courier", "Open Sans", "Book Antiqua", "Helvetica"];
+		var seriousnessArray = ["none","Comic Sans", "Arial", "Courier", "Open Sans", "Book Antiqua", "Helvetica"];
 
 		var result = [];
 		var index;
 		var indexOfNewArray = 0;
-
-	
-
 
 		//if the font is in the list of most to least serious fonts, we get the index. Else, we tell the user we have no suggestions.
 		if ($.inArray(fontInput, seriousnessArray)){
@@ -18,45 +14,31 @@
 			for (var i = 0; i < seriousnessArray.length; i++){
 				if (seriousnessArray[i] == fontInput){
 					index = i;
-				}else{
-
 				}
 			}
 		} else {
-			document.write("Sorry, we could not find fonts more " + fontCharacteristic + " than " + fontInput + ".");
+		
 		}
 
-		//We then get the fonts who are more or less of what the user want it, and create a resulting array with it
+		//We then get the fonts who are more or less of what the user wanted, and create a resulting array with it
 
-	/*	for (var i = index + 1; i < seriousnessArray.length; i++){
-			if (index >= seriousnessArray.length - 1){
-				document.write("Sorry, we could not find fonts more" + fontCharacteristic +  " than " + fontInput + ".");
-			}
-			result[indexOfNewArray] = seriousnessArray[i];
-			indexOfNewArray++;
-		}*/
+		function seriousnessResults(){
 
-			function results(){
+			if (index >= seriousnessArray.length - 1){ //if the font the user looked up is the most serious already
 
+				document.write("Sorry, we could not find fonts more" + fontCharacteristic +  " than " + fontInput);
+
+			} else { // if it isn't, it prints the rest
+					document.write("Fonts more " + fontCharacteristic +  " than " + fontInput + ": ");
+					for (var i = index + 1; i < seriousnessArray.length; i++){
+					result[indexOfNewArray] = seriousnessArray[i];
+					indexOfNewArray++;
+					}
+				
 			
-			if (index >= seriousnessArray.length - 1){
-
-			document.write("Sorry, we could not find fonts more " + fontCharacteristic +  " than " + fontInput);
-
-			}else {
-
-			document.write("Fonts more " + fontCharacteristic +  " than " + fontInput + ": \n");
-			
-			for (var i = index + 1; i < seriousnessArray.length; i++){
-			result[indexOfNewArray] = seriousnessArray[i];
-			indexOfNewArray++;
 			}
-
-		}
-		document.write(result + ".");
+				document.write(result + "."); // change to a for loop to format the results better for each, the name, a comma, and a space
 		}
 
-		results();  //prinits them
 
-		//we print the suggestions
-		//document.write(result);
+		seriousnessResults();  //prints them
