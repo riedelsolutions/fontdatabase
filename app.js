518a6dcb8ff850428b7dbb11e31bbcfc3c835cf7
riedelsolutions <38634824+riedelsolutions@users.noreply.github.com>
2018-07-07 01:46:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,69 @@
 
+
+		//OLD TRIAL PROMPT METHOD
+		//var fontCharacteristic = prompt("I want a font more: " ,"");
+		//var fontInput = prompt("than","");
+		
+
+
+		//Setting up the variables
+		var fontCharacteristic;
+		var fontInput;
+
+		function setCharacteristic(){
+			fontCharacteristic = document.getElementById('characteristic').value;
+			
+		}
+
+		function setFont(){
+			fontInput = document.getElementById('font').value;
+		
+		}
 	
-		var fontCharacteristic = prompt("I want a font more: " ,"");
-		var fontInput = prompt("than","");
 
 		var seriousnessArray = ["none","Comic Sans", "Arial", "Courier", "Open Sans", "Book Antiqua", "Helvetica"];
 
 		var result = [];
 		var index;
 		var indexOfNewArray = 0;
+
+
+
+
+
+		//algorithm
+		function algo(){
+		
+
+			/*if(fontCharacteristic == "formal"){
+
+				//seriousnessarray
+
+			}else if (fontCharacteristic == "informal"){
+
+				//informalarray
+
+			}else if (fontCharacteristic == "serif"){
+
+				//serifarray
+
+			} else if (fontCharacteristic == "sansserif"){
+
+				//sansserifarray
+
+			}else{
+				//nothing.
+			}
+*/
+
+
+
+
+
+
+
+
+
 
 		//if the font is in the list of most to least serious fonts, we get the index. Else, we tell the user we have no suggestions.
 		if ($.inArray(fontInput, seriousnessArray)){
@@ -23,11 +79,17 @@
 
 		//We then get the fonts who are more or less of what the user wanted, and create a resulting array with it
 
-		function seriousnessResults(){
+		function results(){ // CHANGE THIS TO APPLY TO ANY OF THE OTHER CHARACTERISTICS^^^ maybe
+							// a function that returns... function noneFound(){ document.write("Sorry, we could not find fonts more " + fontCharacteristic +  " than " + fontInput);}
 
 			if (index >= seriousnessArray.length - 1){ //if the font the user looked up is the most serious already
+/* new
+				var y = document.createElement('p');
+				y.textContent = "Sorry, we could not find fonts more " + fontCharacteristic +  " than " + fontInput;
+				document.body.appendChild(y);
+*/
+		document.write("Sorry, we could not find fonts more " + fontCharacteristic +  " than " + fontInput);
 
-				document.write("Sorry, we could not find fonts more" + fontCharacteristic +  " than " + fontInput);
 
 			} else { // if it isn't, it prints the rest
 					document.write("Fonts more " + fontCharacteristic +  " than " + fontInput + ": ");
@@ -42,9 +104,9 @@
 		}
 
 
-		seriousnessResults();  //prints them
+		results();  //prints them
 
-
+		
 		/*
 		PSEUDOCODE
 
@@ -62,3 +124,4 @@
 		}	
 
 		*/
+	}
