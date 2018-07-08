92fe63384280d92ec0ae315d8d53bd59557dbcea
riedelsolutions <38634824+riedelsolutions@users.noreply.github.com>
2018-07-07 22:36:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,127 +1,111 @@
-
-
-		//OLD TRIAL PROMPT METHOD
-		//var fontCharacteristic = prompt("I want a font more: " ,"");
-		//var fontInput = prompt("than","");
-		
-
-
-		//Setting up the variables
+	
+//Setting up the variables
 		var fontCharacteristic;
 		var fontInput;
 
+		//Sets the characteristic according to what the user chooses on the 'select' menu.
 		function setCharacteristic(){
 			fontCharacteristic = document.getElementById('characteristic').value;
 			
 		}
 
+		//Sets the font according to what the user chooses on the 'select' menu.
 		function setFont(){
 			fontInput = document.getElementById('font').value;
 		
 		}
-	
 
+//font arrays
 		var seriousnessArray = ["none","Comic Sans", "Arial", "Courier", "Open Sans", "Book Antiqua", "Helvetica"];
+		var informalArray = ["none", "Helvetica", "Book Antiqua", "Open Sans", "Courier", "Arial", "Comic Sans"];
+		var serifArray = [];
+		var sansSerifArray = [];
 
-		var result = [];
-		var index;
-		var indexOfNewArray = 0;
-
-
-
-
-
-		//algorithm
-		function algo(){
-		
-
-			/*if(fontCharacteristic == "formal"){
-
-				//seriousnessarray
-
-			}else if (fontCharacteristic == "informal"){
-
-				//informalarray
-
-			}else if (fontCharacteristic == "serif"){
-
-				//serifarray
-
-			} else if (fontCharacteristic == "sansserif"){
-
-				//sansserifarray
-
-			}else{
-				//nothing.
-			}
-*/
+//Helper variables
+		var result = []; //Array with the results(output).
+		var index;		//Index of where in the original array the results list should start.
+		var indexOfNewArray = 0; //Helps initialize and fill the result[] array.
 
 
 
+//algorithm
 
+		//noneFound(); runs if there are no fonts to be suggested.
+		function noneFound(){ 
+			var noResults = "Sorry, we could not find fonts more " + fontCharacteristic +  " than " + fontInput + ".";
+   			var newParagraph = document.createElement('p');
+   			newParagraph.textContent = noResults;
+   			document.getElementById("resultDiv").appendChild(newParagraph);
+    }
 
+		//createResultsArray(arrayType); creates the new array with the results -> Creates the output.
+		function createResultsArray(arrayType){
+			if ($.inArray(fontInput, arrayType)){
 
-
-
-
-
-		//if the font is in the list of most to least serious fonts, we get the index. Else, we tell the user we have no suggestions.
-		if ($.inArray(fontInput, seriousnessArray)){
-
-			for (var i = 0; i < seriousnessArray.length; i++){
-				if (seriousnessArray[i] == fontInput){
-					index = i;
-				}
-			}
-		} else {
-		
+					for (var i = 0; i < arrayType.length; i++){
+						if (arrayType[i] == fontInput){
+							index = i;
+						}
+					}
+				} 
 		}
 
-		//We then get the fonts who are more or less of what the user wanted, and create a resulting array with it
 
-		function results(){ // CHANGE THIS TO APPLY TO ANY OF THE OTHER CHARACTERISTICS^^^ maybe
-							// a function that returns... function noneFound(){ document.write("Sorry, we could not find fonts more " + fontCharacteristic +  " than " + fontInput);}
+		// results(chosenArray); prints the output.
+		function results(chosenArray){ 
 
-			if (index >= seriousnessArray.length - 1){ //if the font the user looked up is the most serious already
-/* new
-				var y = document.createElement('p');
-				y.textContent = "Sorry, we could not find fonts more " + fontCharacteristic +  " than " + fontInput;
-				document.body.appendChild(y);
-*/
-		document.write("Sorry, we could not find fonts more " + fontCharacteristic +  " than " + fontInput);
+			if (index >= chosenArray.length - 1){ //if the font the user looked up is the most serious already
 
+			noneFound();
 
 			} else { // if it isn't, it prints the rest
-					document.write("Fonts more " + fontCharacteristic +  " than " + fontInput + ": ");
-					for (var i = index + 1; i < seriousnessArray.length; i++){
-					result[indexOfNewArray] = seriousnessArray[i];
+					/* OLD document.write("Fonts more " + fontCharacteristic +  " than " + fontInput + ": ");*/
+					for (var i = index + 1; i < chosenArray.length; i++){
+					result[indexOfNewArray] = chosenArray[i];
 					indexOfNewArray++;
 					}
+
+			var fontsMoreEtc = "Fonts more " + fontCharacteristic +  " than " + fontInput + ": " + result + ".";
+   			var paragraph = document.createElement('p');
+   			paragraph.textContent = fontsMoreEtc;
+   			document.getElementById("resultDiv").appendChild(paragraph);
+    }
 				
 			
 			}
-				document.write(result + "."); // change to a for loop to format the results better for each, the name, a comma, and a space
-		}
-
-
-		results();  //prints them
-
+			
 		
-		/*
-		PSEUDOCODE
 
-		function obtainResult(){
-			if (user chooses "more serious than"){
-				seriousnessResults();
-			} else if (user chooses "more informal than"){
-				informalResults();
-			} else if (user chooses "more serif than"){
-				serifResults();
-			} else if (user chooses "less serif than"){
-				sansSerifResults();
+
+		function algo(){ //Change this name once I get rid of the original results function.
+		
+
+			if(fontCharacteristic == "formal"){ //If the user looks for more formal fonts, we create the serious results array 
+
+
+				createResultsArray(seriousnessArray);
+				results(seriousnessArray);
+				
+			} else if (fontCharacteristic == "informal"){ //If the user looks for more formal fonts, we create the informal results array 
+
+				createResultsArray(informalArray);
+				results(informalArray);
+
+			}else if (fontCharacteristic == "serif"){ //If the user looks for more formal fonts, we create the serif results array 
+
+				createResultsArray(serifArray);
+				results(serifArray);
+
+				
+			} else if (fontCharacteristic == "sansserif"){ //If the user looks for more formal fonts, we create the sans serif results array 
+
+				createResultsArray(sansSerifArray);
+				results(sansSerifArray);
+
+			
+			}else{
+				//nothing.
 			}
 
-		}	
 
-		*/
 	}
